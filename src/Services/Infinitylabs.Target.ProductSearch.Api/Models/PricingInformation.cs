fd019c79ec1d471251f6a46fef4a302ecebb885c
JohnMark Sill <johnmark.sill@infinitylabs.io>
2019-05-08 17:24:45 -0500

    Added mongodb

--- src/Services/Infinitylabs.Target.ProductSearch.Api/Models/PricingInformation.cs
+++ src/Services/Infinitylabs.Target.ProductSearch.Api/Models/PricingInformation.cs
@@ -1,11 +1,15 @@
 using InfinityLabs.Target.ProductSearch.Api.Enums;
+using MongoDB.Bson.Serialization.Attributes;
 
 namespace InfinityLabs.Target.ProductSearch.Api.Models
 {
     public class PricingInformation : BaseModel
     {
+
+        [BsonElement("currency_code")]
         public CurrencyCode CurrencyCode { get; set; }
-        
+
+        [BsonElement("value")]
         public decimal Value { get; set; }
     }
 }
--- src/Services/Infinitylabs.Target.ProductSearch.Api/Models/ProductBase.cs
+++ src/Services/Infinitylabs.Target.ProductSearch.Api/Models/ProductBase.cs
@@ -1,7 +1,12 @@
+using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+
 namespace InfinityLabs.Target.ProductSearch.Api.Models
 {
     public abstract class BaseModel
     {
+        [BsonId]
+        [BsonRepresentation(BsonType.ObjectId)]
         public int Id { get; set; }
     }
 }
