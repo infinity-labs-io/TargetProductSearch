d49b2dd2ee82209f9f3c9eb28ff0f7ee9cc66611
JohnMark Sill <johnmark.sill@infinitylabs.io>
2019-05-08 21:07:59 -0500

    Changed serialization technique of CountryCode

--- src/Services/Infinitylabs.Target.ProductSearch.Api/Models/PricingInformation.cs
+++ src/Services/Infinitylabs.Target.ProductSearch.Api/Models/PricingInformation.cs
@@ -1,5 +1,7 @@
 using InfinityLabs.Target.ProductSearch.Api.Enums;
 using MongoDB.Bson.Serialization.Attributes;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Converters;
 
 namespace InfinityLabs.Target.ProductSearch.Api.Models
 {
@@ -7,6 +9,7 @@ namespace InfinityLabs.Target.ProductSearch.Api.Models
     {
 
         [BsonElement("currency_code")]
+        [JsonConverter(typeof(StringEnumConverter))]
         public CurrencyCode CurrencyCode { get; set; }
 
         [BsonElement("value")]
